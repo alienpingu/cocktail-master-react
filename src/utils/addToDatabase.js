5ee4199fadc08edfb702fc5bb6e8ddac69f37a12
alienpingu <samuele.bertocco@gmail.com>
2022-03-16 21:02:55 +0100

    [favorite] fix btn

--- src/utils/addToDatabase.js
+++ src/utils/addToDatabase.js
@@ -1,14 +1,22 @@
-export default async function addToDatabase  (idDrink)  {
+export default async function addToDatabase  (cocktailJson)  {
     let oldArray = localStorage.getItem("database");
     let realArray = []
-    try {
-        realArray = JSON.parse(oldArray)
-    } catch(ex) {
-        realArray = []
-        console.log('✒️ Creato nuovo DB nel browser locale')
+    let newArray = ''
+    if (oldArray) {
+        try {
+            realArray = JSON.parse(oldArray)
+        } catch(ex) {
+            realArray = []
+            console.log('✒️ Creato nuovo DB nel browser locale')
+        }
+        realArray.push(cocktailJson)
+         newArray = JSON.stringify(realArray)
+
+    } else {
+        realArray.push(cocktailJson)
+        newArray = JSON.stringify(realArray)
     }
-    realArray.push(idDrink)
-    let newArray = JSON.stringify(realArray)
+    
     localStorage.setItem("database", newArray);
     return realArray;
 }
